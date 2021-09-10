0c89e075e55562b20b656c893bc9fe5ec5ef0079
Villarreal Mendieta Carlos Ramiro <43800344+cvillarreal1997@users.noreply.github.com>
2021-09-09 23:22:17 -0400

    valid

--- BQ-Cajas/src/app/components/deposito/deposito.component.ts
+++ BQ-Cajas/src/app/components/deposito/deposito.component.ts
@@ -130,10 +130,44 @@ export class DepositoComponent implements OnInit {
 
     }
   }
+  validadorDeCedula(cedula: String): any {
+ 
+    if (cedula.length == 10) {
+      let tercerDigito = parseInt(cedula.substring(2, 3));
+      if (tercerDigito < 6) {
+        // El ultimo digito se lo considera dígito verificador
+        let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];
+        let verificador = parseInt(cedula.substring(9, 10));
+        let suma: number = 0;
+        let digito: number = 0;
+        for (let i = 0; i < (cedula.length - 1); i++) {
+          digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+          suma += ((parseInt((digito % 10) + '') + (parseInt((digito / 10) + ''))));
+          //      console.log(suma+" suma"+coefValCedula[i]); 
+        }
+        suma = Math.round(suma);
+        //  console.log(verificador);
+        //  console.log(suma);
+        //  console.log(digito);
+        if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10) == verificador)) {
+          return true;
+        } else if ((10 - (Math.round(suma % 10))) == verificador) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+         return false;
+      }
+    } else {
+      return false;
+    }
+   
+
+  }
 
 
 
 
-  
 
 }
